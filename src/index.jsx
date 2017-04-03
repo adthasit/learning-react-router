1b854edfb59b80787a3f7c8b8ad72274b8ad66a0
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-04-03 16:44:04 +0700

    Hello World!!

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!!</h1>
+            </div>
+
+        );
+    }
+}
+
+render(
+    <Home/>, document.getElementById('container'));
